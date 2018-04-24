ddda3d017ae06e6ab6c03622fd450ac846f42381
Jishnu <jishnunambissan@gmail.com>
2018-04-24 07:35:01 +0530

    Adding the arduino code and sample datasets obtained

---
+++ out_analyse.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = open("out4.txt",'r')
+freq = np.array([])
+amp = np.array([])
+for line in f:
+    line = line.strip()
+    if line[0]=='F':
+        freq = np.append(freq,float(line[4:]))
+    else:
+        amp = np.append(amp,float(line[3:]))
+
+plt.plot(freq[0:100],amp[0:100],label="unavg")      
+
+#for i in range(101):
+#    amp[i] = amp[i]+amp[101+i]+amp[202+i]
+#    
+#amp = amp/3.0
+#plt.plot(freq[0:100],amp[0:100],label="avg")
+plt.plot(freq,amp,label="original")
+plt.legend()
+plt.show()
---
+++ si5351_sna_code/si5351_sna_code.ino
@@ -0,0 +1,38 @@
+#include "si5351.h"
+#include "Wire.h"
+
+const int analogInPin = A0;
+
+Si5351 si5351(0x62);
+unsigned long freq;
+unsigned long calfreq = 0ULL; //-7579900ULL; //calibration done with Hantek DSO
+int sensorValue = 0;  
+
+void setup()
+{
+  si5351.init(SI5351_CRYSTAL_LOAD_10PF,25000000, calfreq);
+  freq = 360000000ULL;
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  si5351.set_freq(freq, SI5351_CLK2);
+  delay(100);
+  sensorValue = analogRead(analogInPin);
+ 
+  Serial.print("Freq"); 
+  Serial.println(freq);
+  Serial.print("Rv=");
+  Serial.println(sensorValue);
+  /*if (freq< 1100000000ULL)
+    freq += 1000000ULL;
+  else
+    freq = 1000000000ULL;*/
+    if (freq< 500000000ULL)
+    freq += 1000000ULL;
+  else
+    freq = 300000000ULL;
+   
+}
+
